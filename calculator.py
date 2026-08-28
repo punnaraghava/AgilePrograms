dbcdb7b5508bf35852dbecc86933c65ab4706eb4
Raghava <punnaraghava625@gmail.com>
2026-08-28 13:16:24 +0530

    committed successfully

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ZeroDivisionError("Cannot divide by zero")  # match your test
+    return a / b
+
